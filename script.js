8751fc8382453ee4e537d5e5cadb31bcc83b7d37
Hemalatha0506 <hemalathaneelapu05@gmail.com>
2026-06-15 12:46:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("profileImage").src =
+"https://randomuser.me/api/portraits/women/44.jpg";
+
+document.getElementById("patientInfo").innerHTML = `
+<h2>Jessica Taylor</h2>
+
+<p><strong>Gender:</strong> Female</p>
+
+<p><strong>Date of Birth:</strong> August 23, 1996</p>
+
+<p><strong>Phone:</strong> (415) 555-1234</p>
+
+<p><strong>Emergency Contact:</strong> (415) 555-5678</p>
+
+<p><strong>Insurance:</strong> Sunrise Health Assurance</p>
+`;
